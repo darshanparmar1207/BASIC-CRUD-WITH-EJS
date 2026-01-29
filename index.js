383117e698c8eaf496d5f9a3c129e78a00e8680a
Darshan Parmar <parmardarshanr1@gmail.com>
2026-01-29 18:22:23 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const userModel = require('./models/user')
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req,res) => {
+    res.render("index");
+})
+
+app.get('/read', async(req,res) => {
+    let allusers = await userModel.find()
+    res.render("read" , {users: allusers});
+})
+
+
+
+// that is post data 
+app.post('/create', async (req,res) => {
+    let {name, email, image} = req.body;
+
+    let createdUser = await userModel.create({
+        name,
+        email,
+        image
+    });
+
+    res.redirect("/read")
+})
+
+// that is delete data
+app.get('/delete/:id', async(req,res) => {
+    let allusers = await userModel.findOneAndDelete({_id: req.params.id})
+    res.redirect("/read");
+})
+
+app.listen(3000);
